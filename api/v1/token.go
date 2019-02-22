5277f8cdc0307ff915a0281880a67d388ea286dc
wlchn <wlchn@qq.com>
2019-02-22 16:34:29 +0800

    add top token filter api

--- api/v1/token.go
+++ api/v1/token.go
@@ -26,6 +26,25 @@ func GetTokens(c *gin.Context) {
 	}
 }
 
+func GetTokensTop(c *gin.Context) {
+	orderQuery := c.DefaultQuery("order", "volume_24h")
+
+	order := "volume_24h desc"
+	if orderQuery == "volume_24h" || orderQuery == "volume_7d" || orderQuery == "volume_all" {
+		order = orderQuery + " desc"
+	}
+
+	pageSize := 10
+	offset := 0
+
+	var tokens []models.Token
+	if err := models.DB.Offset(offset).Limit(pageSize).Order(order).Find(&tokens).Error; gorm.IsRecordNotFoundError(err) {
+		c.AbortWithStatus(404)
+	} else {
+		c.JSON(200, tokens)
+	}
+}
+
 func GetToken(c *gin.Context) {
 	address := c.Params.ByName("address")
 	token := models.Token{}
--- api/v1/v1.go
+++ api/v1/v1.go
@@ -9,14 +9,15 @@ func ApplyRoutes(r *gin.RouterGroup) {
 	{
 		v1.GET("/tokens", GetTokens)
 		v1.GET("/tokens/:address", GetToken)
+		v1.GET("/tokens_top", GetTokensTop)
 
 		v1.GET("/relayers", GetRelayers)
 		v1.GET("/relayers/:slug", GetRelayer)
 
 		v1.GET("/trades", GetTrades)
+		v1.GET("/trades/:uuid", GetTrade)
 		v1.GET("/trades_latest", GetTradesLatest)
 		v1.GET("/trades_chart", GetTradesChart)
 		v1.GET("/trades_indicators", GetTradesIndicators)
-		v1.GET("/trades/:uuid", GetTrade)
 	}
 }
