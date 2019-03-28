f26c7cfb63b32a57d4be4f580c7256c228fd40cc
wlchn <wlchn@qq.com>
2019-03-28 17:29:55 +0800

    add monitoring

--- api/api.go
+++ api/api.go
@@ -6,6 +6,11 @@ import (
 )
 
 func ApplyRoutes(r *gin.Engine) {
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 	api := r.Group("/api")
 	{
 		apiv1.ApplyRoutes(api)
