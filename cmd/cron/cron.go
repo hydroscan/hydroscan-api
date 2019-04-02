e8afe97d576903b2222d246856dd2ff26167f66b
wlchn <wlchn@qq.com>
2019-04-02 19:49:17 +0800

    remove once code

--- cmd/cron/cron.go
+++ cmd/cron/cron.go
@@ -31,11 +31,6 @@ func main() {
 	redis.Connect()
 	task.InitEthClient()
 
-	// correct historical data once
-	go func() {
-		task.FetchHistoricalLogs(true)
-	}()
-
 	log.Info("cron running")
 
 	gocron.Every(1).Minute().Do(safeTask(task.FetchRecentLogs))
